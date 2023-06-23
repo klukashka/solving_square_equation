0db6e9402ec79d8f0b434b2797ef2dd384b369ec
klukashka <klukin0202@yandex.ru>
2023-06-23 19:15:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a, b, c = map(int, input().split())
+if a != 0:
+    d = b**2 - 4*a*c
+    print((-b + d**0.5)/(2*a), (-b - d**0.5)/(2*a))
+elif b != 0:
+    print(-c/b)
+else:
+    print('any x in R')
